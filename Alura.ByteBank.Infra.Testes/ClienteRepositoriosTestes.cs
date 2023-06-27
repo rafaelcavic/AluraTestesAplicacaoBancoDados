801e0d626d0f1fed5b8f226f05992ad0cc2b67c7
Rafael Cavic <rafaelcavic@hotmail.com>
2023-06-26 22:08:52 -0300

    02. Testes de Integração (1 a 4)

---
+++ Alura.ByteBank.Infra.Testes/ClienteRepositoriosTestes.cs
@@ -0,0 +1,25 @@
+﻿using Alura.ByteBank.Dados.Repositorio;
+using Alura.ByteBank.Dominio.Entidades;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace Alura.ByteBank.Infra.Testes
+{
+    public class ClienteRepositoriosTestes
+    {
+        [Fact]
+        public void TestaObterTodosClientes()
+        {
+            var _repositorio = new ClienteRepositorio();
+
+            List<Cliente> lista = _repositorio.ObterTodos();
+
+            Assert.NotNull(lista);
+            Assert.Equal(3, lista.Count);
+        }
+    }
+}
